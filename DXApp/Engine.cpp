b783282f7d269ab96803fac5584e2c2ff8f7acc8
Jakub Konka <jakub.konka@strath.ac.uk>
2015-09-23 13:58:34 +0100

    Create standard DX swap chain

--- DXApp/Engine.cpp
+++ DXApp/Engine.cpp
@@ -2,6 +2,8 @@
 #include "Engine.h"
 #include "DXHelper.h"
 
+using DXHelper::ThrowIfFailed;
+
 void Engine::Initialize()
 {
 	ComPtr<ID3D11Device> device;
@@ -19,9 +21,31 @@ void Engine::Initialize()
 		nullptr,
 		&devContext
 		);
-	DXHelper::ThrowIfFailed(hr);
-	hr = device.As(&device_);
-	hr = devContext.As(&devContext_);
+	ThrowIfFailed(hr);
+	ThrowIfFailed(device.As(&device_));
+	ThrowIfFailed(devContext.As(&devContext_));
+
+	ComPtr<IDXGIDevice1> dxgiDevice;
+	ThrowIfFailed(device_.As(&dxgiDevice));
+	ComPtr<IDXGIAdapter> dxgiAdapter;
+	ThrowIfFailed(dxgiDevice->GetAdapter(&dxgiAdapter));
+	ComPtr<IDXGIFactory2> dxgiFactory;
+	ThrowIfFailed(dxgiAdapter->GetParent(__uuidof(IDXGIFactory2), &dxgiFactory));
+
+	DXGI_SWAP_CHAIN_DESC1 scd = { 0 };
+	scd.BufferCount = 2;
+	scd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+	scd.Format = DXGI_FORMAT_B8G8R8A8_UNORM;
+	scd.SwapEffect = DXGI_SWAP_EFFECT_FLIP_SEQUENTIAL;
+	scd.SampleDesc.Count = 1;
+
+	CoreWindow^ window = CoreWindow::GetForCurrentThread();
+	ThrowIfFailed(dxgiFactory->CreateSwapChainForCoreWindow(
+		device_.Get(),
+		reinterpret_cast<IUnknown*>(window),
+		&scd,
+		nullptr,
+		&swapChain_));
 }
 
 void Engine::Update()
@@ -31,5 +55,5 @@ void Engine::Update()
 
 void Engine::Render()
 {
-
+	ThrowIfFailed(swapChain_->Present(1, 0));
 }
