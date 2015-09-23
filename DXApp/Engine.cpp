8f9aba013ac4af9878fdcf1a88cc4043951d8d3a
Jakub Konka <jakub.konka@strath.ac.uk>
2015-09-23 15:40:37 +0100

    Draw simple triangle

--- DXApp/Engine.cpp
+++ DXApp/Engine.cpp
@@ -1,11 +1,13 @@
 #include "pch.h"
 #include "Engine.h"
 #include "DXHelper.h"
+#include <fstream>
 
 using DXHelper::ThrowIfFailed;
 
 void Engine::Initialize()
 {
+	// initialise Direct3D
 	ComPtr<ID3D11Device> device;
 	ComPtr<ID3D11DeviceContext> devContext;
 
@@ -25,6 +27,7 @@ void Engine::Initialize()
 	ThrowIfFailed(device.As(&device_));
 	ThrowIfFailed(devContext.As(&devContext_));
 
+	// create the swap chain
 	ComPtr<IDXGIDevice1> dxgiDevice;
 	ThrowIfFailed(device_.As(&dxgiDevice));
 	ComPtr<IDXGIAdapter> dxgiAdapter;
@@ -47,12 +50,25 @@ void Engine::Initialize()
 		nullptr,
 		&swapChain_));
 
+	// set the render target
 	ComPtr<ID3D11Texture2D> backBuffer;
 	ThrowIfFailed(swapChain_->GetBuffer(0, __uuidof(ID3D11Texture2D), &backBuffer));
 	ThrowIfFailed(device_->CreateRenderTargetView(
 		backBuffer.Get(),
 		nullptr,
 		&renderTarget_));
+
+	// set the viewport
+	D3D11_VIEWPORT viewport = { 0 };
+	viewport.TopLeftX = 0;
+	viewport.TopLeftY = 0;
+	viewport.Width = window->Bounds.Width;
+	viewport.Height = window->Bounds.Height;
+	devContext_->RSSetViewports(1, &viewport);
+
+	// initialise graphics and pipeline
+	InitGraphics();
+	InitPipeline();
 }
 
 void Engine::Update()
@@ -68,5 +84,98 @@ void Engine::Render()
 	float colour[4] = { 0.0f, 0.2f, 0.4f, 1.0f };
 	devContext_->ClearRenderTargetView(renderTarget_.Get(), colour);
 
+	// set the vertex buffer
+	UINT stride = sizeof(VERTEX);
+	UINT offset = 0;
+	devContext_->IASetVertexBuffers(
+		0,
+		1,
+		vertexBuffer_.GetAddressOf(),
+		&stride,
+		&offset);
+
+	// set the primitive topology
+	devContext_->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+
+	// draw...
+	devContext_->Draw(3, 0);
+
+	// switch the buffers
 	ThrowIfFailed(swapChain_->Present(1, 0));
+}
+
+void Engine::InitGraphics()
+{
+	VERTEX verticesOfTriangle[] =
+	{
+		{ 0.0f, 0.5f, 0.0f },
+		{ 0.45f, -0.5f, 0.0f },
+		{ -0.45f, -0.5f, 0.0f },
+	};
+
+	D3D11_BUFFER_DESC bd = { 0 };
+	bd.ByteWidth = sizeof(VERTEX) * ARRAYSIZE(verticesOfTriangle);
+	bd.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+
+	D3D11_SUBRESOURCE_DATA srd = { verticesOfTriangle, 0, 0 };
+
+	ThrowIfFailed(device_->CreateBuffer(&bd, &srd, &vertexBuffer_));
+}
+
+void Engine::InitPipeline()
+{
+	Array<byte>^ vsFile = LoadShaderFile("VertexShader.cso");
+	Array<byte>^ psFile = LoadShaderFile("PixelShader.cso");
+
+	ThrowIfFailed(device_->CreateVertexShader(
+		vsFile->Data,
+		vsFile->Length,
+		nullptr,
+		&vertexShader_));
+	ThrowIfFailed(device_->CreatePixelShader(
+		psFile->Data,
+		psFile->Length,
+		nullptr,
+		&pixelShader_));
+
+	devContext_->VSSetShader(vertexShader_.Get(), nullptr, 0);
+	devContext_->PSSetShader(pixelShader_.Get(), nullptr, 0);
+
+	D3D11_INPUT_ELEMENT_DESC ied[] =
+	{
+		{ "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0 },
+	};
+
+	ThrowIfFailed(device_->CreateInputLayout(
+		ied,
+		ARRAYSIZE(ied),
+		vsFile->Data,
+		vsFile->Length,
+		&inputLayout_));
+
+	devContext_->IASetInputLayout(inputLayout_.Get());
+}
+
+Array<byte>^ Engine::LoadShaderFile(std::string file)
+{
+	Array<byte>^ fileData = nullptr;
+
+	// open file
+	std::ifstream vertexFile(
+		file,
+		std::ios::in | std::ios::binary | std::ios::ate);
+
+	if (vertexFile.is_open())
+	{
+		// get the length of the file
+		int length = (int)vertexFile.tellg();
+
+		// collect the file data
+		fileData = ref new Array<byte>(length);
+		vertexFile.seekg(0, std::ios::beg);
+		vertexFile.read(reinterpret_cast<char*>(fileData->Data), length);
+		vertexFile.close();
+	}
+
+	return fileData;
 }
--- DXApp/Engine.h
+++ DXApp/Engine.h
@@ -3,17 +3,31 @@
 
 using namespace Microsoft::WRL;
 using namespace Windows::UI::Core;
+using namespace Platform;
+
+typedef struct VERTEX
+{
+	float X, Y, Z;
+} VERTEX;
 
 class Engine
 {
-private:
-	ComPtr<ID3D11Device1> device_;
-	ComPtr<ID3D11DeviceContext1> devContext_;
-	ComPtr<IDXGISwapChain1> swapChain_;
-	ComPtr<ID3D11RenderTargetView> renderTarget_;
-
 public:
 	void Initialize();
 	void Update();
 	void Render();
+
+private:
+	void InitGraphics();
+	void InitPipeline();
+	Array<byte>^ LoadShaderFile(std::string);
+
+	ComPtr<ID3D11Device1> device_;
+	ComPtr<ID3D11DeviceContext1> devContext_;
+	ComPtr<IDXGISwapChain1> swapChain_;
+	ComPtr<ID3D11RenderTargetView> renderTarget_;
+	ComPtr<ID3D11Buffer> vertexBuffer_;
+	ComPtr<ID3D11VertexShader> vertexShader_;
+	ComPtr<ID3D11PixelShader> pixelShader_;
+	ComPtr<ID3D11InputLayout> inputLayout_;
 };
